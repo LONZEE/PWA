f70050cbc8fc9d339171a1d1969918e2aa2cb6cc
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-10 13:21:02 -0500

    added some logic to install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,7 +2,15 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt fired');
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Show the install button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
